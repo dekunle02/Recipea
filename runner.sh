1b9f81324e49252db5b78b08c048e9ea9d19e42b
dekunle02 <dekunle02@gmail.com>
2022-12-24 19:36:03 +0000

    fix:update to bash script

--- runner.sh
+++ runner.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+cd $SCRIPT_DIR
 git pull origin main
 npm install 
 open "http://localhost:3000"
